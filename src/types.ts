ba3a077832965a8602541ee46a303e27933acdf6
ModyQyW <wurui-dev@foxmail.com>
2022-11-22 21:20:17 +0800

    feat: update types

---
+++ src/types.ts
@@ -0,0 +1,50 @@
+export interface SimpleOptions {
+  success?: (result: any) => void;
+  fail?: (error: any) => void;
+}
+
+export interface TaskPromise<T = UniApp.GeneralCallbackResult> extends Promise<T> {
+  abort: () => void;
+  onHeadersReceived: (callback: (result: { header: Record<string, any> }) => void) => void;
+  offHeadersReceived: (callback: (result: { header: Record<string, any> }) => void) => void;
+}
+
+export interface RequestPromise<T = UniApp.RequestSuccessCallbackResult> extends TaskPromise<T> {
+  onChunkReceived: (callback: (result: { data: ArrayBuffer }) => void) => void;
+  offChunkReceived: (callback?: (result: { data: ArrayBuffer }) => void) => void;
+}
+
+export interface DownloadFilePromise<T = UniApp.DownloadSuccessData> extends TaskPromise<T> {
+  onProgressUpdate: (
+    callback: (result: {
+      progress: number;
+      totalBytesWritten: number;
+      totalBytesExpectedToWrite: number;
+    }) => void,
+  ) => void;
+  offProgressUpdate: (
+    callback: (result: {
+      progress: number;
+      totalBytesWritten: number;
+      totalBytesExpectedToWrite: number;
+    }) => void,
+  ) => void;
+}
+
+export interface UploadFilePromise<T = UniApp.UploadFileSuccessCallbackResult>
+  extends TaskPromise<T> {
+  onProgressUpdate: (
+    callback: (result: {
+      progress: number;
+      totalBytesSent: number;
+      totalBytesExpectedToSend: number;
+    }) => void,
+  ) => void;
+  offProgressUpdate: (
+    callback: (result: {
+      progress: number;
+      totalBytesSent: number;
+      totalBytesExpectedToSend: number;
+    }) => void,
+  ) => void;
+}
