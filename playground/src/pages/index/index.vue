92c6ec82915703d396dd1b1aa5230bcc4f8ffca8
ModyQyW <wurui-dev@foxmail.com>
2023-01-10 16:58:46 +0800

    chore: add example

---
+++ playground/src/pages/index/index.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { getClipboardData } from '@uni-helper/uni-promises';
+const title = ref('Hello');
+
+getClipboardData({
+  success: (result) => {
+    console.log('success result', result);
+  },
+}).then((result) => {
+  console.log('then result', result);
+});
+</script>
+
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
