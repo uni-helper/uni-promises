4705c5c9821c50054eed4b2717d47221f1378a78
Panpan <wurui.dev@gmail.com>
2025-12-18 21:48:10 +0800

    chore: update deps, support oidc

--- .simple-git-hooks.cjs
+++ .simple-git-hooks.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  "pre-commit": "npx lint-staged",
-  "commit-msg": "npx commitlint --edit ${1}",
+  'pre-commit': 'npx lint-staged --allow-empty',
+  'commit-msg': 'npx commitlint --edit ${1}',
 };
